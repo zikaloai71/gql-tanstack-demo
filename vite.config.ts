a9151d5b9798ea347a7dd7c090a008af637ef24e
zikaloai71 <ze.lo.moukhtar@el-eng.menofia.edu.eg>
2024-12-19 15:10:58 +0200

    log any typescript error in terminal

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import Checker from 'vite-plugin-checker';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Checker({
+      // Enable TypeScript checking
+      typescript: true,
+      
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  }
 });
